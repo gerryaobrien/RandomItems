4e4a8f07aafe797b408b6973387c822a9c202906
Gerry O'Brien <gerry.obrien@outlook.com>
2015-07-26 12:26:08 -0700

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Gerry O'Brien on 7/26/15.
+//  Copyright (c) 2015 Gerry O'Brien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
